c0d4566bcfcdec56beeb89463f83deab5e241b75
mojaichan <ayanokoujibou@hotmail.com>
2014-05-27 15:48:03 +0800

    Production configuration

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 //phpinfo();
-$localurl= "http://".$_SERVER["HTTP_HOST"]."/fern_wcs/";
-//$localurl= "https://www.williamcheng-son.com/customer_portal/";
+//$localurl= "http://".$_SERVER["HTTP_HOST"]."/fern_wcs/";
+$localurl= "https://www.williamcheng-son.com/customer_portal/";
 //$localurl= "https://www.williamcheng-son.com/staging_test/";
 //$cs_email = "david@fern.com.hk";
 $cs_email = "appointment@williamcheng-son.com";
@@ -12,7 +12,7 @@ $replyto_email = "appointment@williamcheng-son.com";
 
 if ($_SERVER["HTTPS"] != "on" || !isset($_SERVER["HTTPS"])){
 	//header('location:'.$localurl.$_SERVER['REQUEST_URI']);
-	//header('location:'."https://".$_SERVER["SERVER_NAME"].$_SERVER['REQUEST_URI']);
+	header('location:'."https://".$_SERVER["SERVER_NAME"].$_SERVER['REQUEST_URI']);
 }
 
 ?>
