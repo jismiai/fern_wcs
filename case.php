8f577e430c99ce26c8719db5b9ae5543fe7549a3
mojaichan <ayanokoujibou@hotmail.com>
2014-06-11 18:28:40 +0800

    20140611 - case update

--- case.php
+++ case.php
@@ -1,22 +1,31 @@
 <?php
-require_once 'controllers/log_control.php';
+//require_once 'controllers/log_control.php';
 
-//if 
+//handle get headers
+$get_caseType = (isset($_GET["type"]))? $_GET["type"] : "";
+$get_caseSubType = (isset($_GET["subtype"]))? $_GET["subtype"] : "";
+$get_showForm = (isset($_GET["type"]) && isset($_GET["subtype"])) ? true : false;
 
 include("templates/head_tag.php");
+include_once ("functions/case/case_lists.php");
+$caseObj = new netsuiteCase();
 ?>
 
+<?php if (!($get_showForm)){ ?>
 <div class="panel panel-default">
 	<div class="panel-heading">Online Cases</div>
 	<div class="panel-body">What would you like to do?</div>
 	<ul class="list-group">
 		<li class="list-group-item">Order non-shirt items</li>
-		<li class="list-group-item">Request swatches and catalogues</li>
+		<li class="list-group-item" ><a href="case.php?type=2&subtype=1">Request swatches and catalogues</a></li>
 		<li class="list-group-item">Check order progress</li>
 		<li class="list-group-item">Complaint/Alteration Request</li>
 		<li class="list-group-item">General Inquiry</li>
+		<li class="list-group-item"><a href="portal.php" class="btn btn-link">Back</a></li>
+		
 	</ul>
 </div>
+<?php } else {?>
 <form name="case_form" id="case_form" class="form-horizontal" role="form" method="post">
 	<fieldset>
 		<legend>General information</legend>
@@ -45,13 +54,26 @@ include("templates/head_tag.php");
 			</div>
 		</div>
 		<div class="form-group">
-		<div class="col-md-4 col-md-offset-4">
-			<input type="submit" class="btn btn-wcs-default" value="Submit" />
-			<a href="portal.php" class="btn btn-link">Back</a>
+			<label for="case_type" class="col-sm-4 control-label">Case Type:</label>
+			<div class="col-sm-8">
+				<?php $caseObj->caseTypeHTML($get_caseType); ?>
+			</div>
 		</div>
-	</div>
+		<div class="form-group">
+			<label for="case_type" class="col-sm-4 control-label">Case Sub-type:</label>
+			<div class="col-sm-8">
+				<?php $caseObj->subTypeHTML($get_caseType,$get_caseSubType); ?>
+			</div>
+		</div>
+		<div class="form-group">
+			<div class="col-md-4 col-md-offset-4">
+				<input type="submit" class="btn btn-wcs-default" value="Submit" />
+				<a href="portal.php" class="btn btn-link">Back</a>
+			</div>
+		</div>
 	</fieldset>
 </form>
+<?php } ?>
 
 <?php 
 include("templates/footer_tag.php");
--- functions/case/case_lists.php
+++ functions/case/case_lists.php
@@ -1,5 +1,77 @@
 <?php
-function(){
-	
+class netsuiteCase {
+	public $caseType = array('2' => 'Order','4'=>'Alteration','3'=>'Complaint','1'=>'General Enquiry');
+	public $subType = array(
+			'1' => array('parent' => '2', 'value' => 'Request for swatch and catalogue'),
+			'2' => array('parent' => '2', 'value' => 'Product Pricing'),
+			'3' => array('parent' => '2', 'value' => 'Fabric and product styles'),
+			'4' => array('parent' => '2', 'value' => 'Order progress'),
+			'5' => array('parent' => '4', 'value' => 'Pricing'),
+			'6' => array('parent' => '4', 'value' => 'Timing'),
+			'7' => array('parent' => '4', 'value' => 'Policies and procedures'),
+			'8' => array('parent' => '4', 'value' => 'Alteration progress'),
+			'9' => array('parent' => '3', 'value' => 'Remake'),
+			'10' => array('parent' => '3', 'value' => 'Local Treatment'),
+			'11' => array('parent' => '1', 'value' => 'Shop appointment'),
+			//'12' => array('parent' => '1', 'value' => 'Others'),
+			'13' => array('parent' => '2', 'value' => 'Place Order'),
+			'14' => array('parent' => '2', 'value' => 'Order progress'),
+			'15' => array('parent' => '4', 'value' => 'Alteration request'),
+			'16' => array('parent' => '3', 'value' => 'Return and Refund'),
+			'17' => array('parent' => '1', 'value' => 'Trip schedule'),
+			'18' => array('parent' => '1', 'value' => 'Others'),
+			'19' => array('parent' => '3', 'value' => 'Alteration')
+			);
+	public $productType = array(
+			'1' => 'Shirt',
+			'2' => 'Suit with Trousers/Pant',
+			'3' => 'Vest',
+			'4' => 'Topcoat',
+			'5' => 'Suit with Skirt',
+			'6' => 'Jacket',
+			'7' => 'Trousers'
+	);
+	public $fabricBrand = array();
+	public $fabricColor = array('White','Black','Grey','Blue','Brown','Red','Yellow','Khaki','Green','Others');
+	public $fabricPattern = array('Plain','Stripes','Check','Others');
+	public $fabricMaterial = array('Cotton','Wool','Others');
+
+	function caseTypeHTML($typeId){
+		echo '<select class="form-control">';
+		foreach ($this->caseType as $key => $value){
+			if ($key ==$typeId){
+				echo '<option selected="selected" value="'.$key.'">'.$value.'</option>';
+			} else {
+				echo '<option value="'.$key.'">'.$value.'</option>';
+			}
+		}
+		echo '</select>';
+	}
+	function subTypeHTML($parentId,$typeId){
+		echo '<select class="form-control">';
+		foreach ($this->subType as $key => $value){
+			if ($value['parent'] == $parentId || empty($parentId)){
+				if ($key ==$typeId){
+					echo '<option selected="selected" value="'.$key.'">'.$value["value"].'</option>';
+				} else {
+					echo '<option value="'.$key.'">'.$value["value"].'</option>';
+				}
+			}
+		}
+		echo '</select>';
+	}
+	function productTypeHTML($typeId){
+		echo '<select class="form-control">';
+		foreach ($this->productType as $key => $value){
+			if ($key ==$typeId){
+				echo '<option selected="selected" value="'.$key.'">'.$value.'</option>';
+			} else {
+				echo '<option value="'.$key.'">'.$value.'</option>';
+			}
+		}
+		echo '</select>';
+	}
 }
+
+
 ?>
